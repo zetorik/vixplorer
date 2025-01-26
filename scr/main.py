536bc45e602823c051ac3099895e1230984d7547
Bob <zetorikk@gmail.com>
2025-01-26 19:11:34 +0200

    Refacor, update

--- scr/main.py
+++ scr/main.py
@@ -1,6 +1,7 @@
 from colorama import Back, Fore, Style
 
 from kbhit import KBHit
+from utils import clamp
 
 import os, shutil
 import sys
@@ -20,10 +21,14 @@ class Explorer:
         self.selected_file = None
         self.is_dir_selected = False
         self.files = []
-        
+
+        self.prev_c = None
+
         self.motion_mult = 1
 
         self.input_buffer = ""
+
+        self.waiting_for_key = False
 
         self.update()
 
@@ -48,11 +53,11 @@ class Explorer:
             print(file)
     
     def move_focus_down(self):
-        self.selected_index = min(self.selected_index + self.motion_mult, len(self.files) -1)
+        self.selected_index = clamp(self.selected_index + self.motion_mult, 0, len(self.files) - 1) 
         self.update()
 
     def move_focus_up(self):
-        self.selected_index = max(self.selected_index - self.motion_mult, 0)
+        self.selected_index = clamp(self.selected_index - self.motion_mult, 0, len(self.files) - 1) 
         self.update()
 
     def delete_selected(self):
@@ -82,47 +87,86 @@ class Explorer:
         self.update()
 
     def dir_up(self):
-        self.working_dir = os.path.dirname(self.working_dir)
+        for i in range(abs(self.motion_mult)):
+            self.working_dir = os.path.dirname(self.working_dir)
 
         self.selected_index = 0
         self.update()
 
     def single_input(self, c: str) -> bool:
         match c:
-            case "j":
-                self.move_focus_down()
-            case "k":
-                self.move_focus_up()
-            case "d":
-                if not self.selected_file:
-                    return False
-
-                self.delete_selected()
             case " ":
                 if not self.selected_file:
                     return False
 
                 self.open_selected()
             case "-":
-                self.dir_up()
-            case "G":
-                self.selected_index = max(len(self.files) - 1, 0)
-                self.update()
+                self.dir_up()           
             case _:
                 return False
-        
+
+        self.motion_mult = 1 
         return True
+    
+    def operator_input(self, o: str):
+        match o:
+            case "d":
+                if not self.selected_file:
+                    return False
+                
+                self.delete_selected()
+            case _:
+                self.move_focus_down()
+
+        
+        self.motion_mult = 1
 
     def buffer_input(self) -> bool:
         ib = self.input_buffer
 
-        if ib.endswith("gg"):
-            self.selected_index = 0
-            self.update()
-        else:
-            return False 
-         
-        return True
+        operators = ("dd")
+        motions = ("gg", "j", "k", "G")
+        found_buffer = False
+
+        for motion in motions:
+            if not ib.endswith(motion):
+                continue
+            
+            found_buffer = True
+
+            match motion:
+                case "gg":
+                    self.motion_mult = 0 - self.selected_index
+                case "G":
+                    self.motion_mult = len(self.files) - 1 - self.selected_index
+                case "j":
+                    pass
+                case "k":
+                    self.motion_mult *= -1
+
+            operator = ib.removesuffix(motion)
+
+            if len(operator) != 0:
+                operator = ib.removesuffix(motion)[-1]
+
+            self.operator_input(operator)
+
+            break
+
+        if not found_buffer:
+            for operator in operators:
+                if not ib.endswith(operator):
+                    continue
+
+                found_buffer = True
+
+                match operator:
+                    case "dd":
+                        self.delete_selected()
+
+                break
+
+        return found_buffer
 
     def input(self, c: str): 
         found_single = self.single_input(c)
@@ -152,10 +196,14 @@ class Explorer:
                 break
 
             if c.isdigit():
-                self.motion_mult += int(c)
+                if not self.prev_c.isdigit():
+                    self.motion_mult *= int(c)
+                else:
+                    self.motion_mult = self.motion_mult * 10 + int(c)
             else:
                 self.input(c)
-                self.motion_mult = 1
+            
+            self.prev_c = c
                    
         self.kb.set_normal_term()
 
